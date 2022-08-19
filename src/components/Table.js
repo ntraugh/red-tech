38887f9a835ca600994e723fbbb7775dd8802719
Nate Traugh <n.traugh@yahoo.com>
2022-08-19 15:49:37 -0400

    switch to fetch request, got data to populate in table rows using getRowId function and calling the correct orderId from the API data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import axios from "axios"
 
@@ -6,10 +6,10 @@ import axios from "axios"
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'OrderId', headerName: 'Order ID', width: 130 },
-  { field: 'CreationDate', headerName: 'Creation Date', width: 130 },
+  { field: 'orderId', headerName: 'Order ID', width: 130 },
+  { field: 'createdDate', headerName: 'Creation Date', width: 130 },
   {
-    field: 'CreatedBy',
+    field: 'createdByUserName',
     headerName: 'Created By',
     type: 'string',
     width: 90,
@@ -21,40 +21,33 @@ const columns = [
 
   },
   {
-    field: 'customer',
+    field: 'customerName',
     headerName: 'Customer',
     type: 'string',
 
   },
 ];
 
-const rows = [
-  { id: 1, CreationDate: 'Snow', OrderId: 'Jon', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 2, CreationDate: 'Lannister', OrderId: 'Cersei', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 3, CreationDate: 'Lannister', OrderId: 'Jaime', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 4, CreationDate: 'Stark', OrderId: 'Arya', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 5, CreationDate: 'Targaryen', OrderId: 'Daenerys', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 6, CreationDate: 'Melisandre', OrderId: null, CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 7, CreationDate: 'Clifford', OrderId: 'Ferrara', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 8, CreationDate: 'Frances', OrderId: 'Rossini', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-  { id: 9, CreationDate: 'Roxie', OrderId: 'Harvey', CreatedBy: "Name", orderType: "Standard", customer: "Kroger" },
-];
+
 
 
 const Table = () => {
+    const [tableData, setTableData] = useState([])
+
     useEffect(() => {
-        const getOrders = async () => {
-            const res = await axios.get("https://red-candidate-web.azurewebsites.net/api/Orders/", { headers: { "ApiKey": "b7b77702-b4ec-4960-b3f7-7d40e44cf5f4"}})
-            console.log(res.data)
-        }
-        getOrders()
+        
+            fetch("https://red-candidate-web.azurewebsites.net/api/Orders/", { headers: { "ApiKey": "b7b77702-b4ec-4960-b3f7-7d40e44cf5f4"}})
+            .then((res) => res.json())
+            .then((res) => setTableData(res))
+      
     }, [])
   return (
     <div style={{ height: 400, width: '100%', boxShadow: "6px 6px 10px black" }}>
       <DataGrid
-      rows={rows}
+      rows={tableData}
       columns={columns}
       pageSize={5}
+      getRowId={(row) => row.orderId}
       rowsPerPageOptions={[5]}
       checkboxSelection/>
     </div>
